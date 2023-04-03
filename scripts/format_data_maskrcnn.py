a668a82dab04043af4549ce42f065eda2b460770
Thomas James Patton <tjp93@g2-login-02.coecis.cornell.edu>
2023-04-03 12:32:11 -0400

    slurm scripts

--- scripts/format_data_maskrcnn.py
+++ scripts/format_data_maskrcnn.py
@@ -4,9 +4,8 @@ import time
 from PIL import Image 
 import numpy as np
 import cv2
-
-import torch 
-import torchvision.transforms as T
+import torch
+from tqdm import tqdm
 
 TRAINING_SET_VERSION = '1'
 TRAINING_DATA_DIR = '../data/training/v{}'.format(TRAINING_SET_VERSION)
@@ -17,9 +16,7 @@ MASKS_DIR = os.path.join(TRAINING_DATA_DIR, 'masks')
 t0 = time.time()
 examples = os.listdir(IMAGES_DIR)
 N = len(examples)
-for i in range(N):
-    if i % 100 == 0:
-        print(float(i/15000.))
+for i in tqdm(range(N)):
 
     # load image and corresponding mask
     image_filename = examples[i]
@@ -33,12 +30,12 @@ for i in range(N):
     image = np.array(Image.open(image_path).convert('RGB'))
     mask = np.array(Image.open(mask_path).convert('L'))
 
-    t1 = time.time()
-    print(t1-t0)
-
     # extract bbox, labels 
     boxes, labels, binary_masks = [], [], []
     class_labels = torch.unique(torch.tensor(mask)).tolist()[1:]    # exclude zero
+    if len(class_labels) == 0:     # if there are no objects annotated in the scene
+        continue
+
     for label in class_labels:
         class_mask = (mask == label).astype('uint8') 
         contours, _ = cv2.findContours(class_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -47,19 +44,18 @@ for i in range(N):
         class_object_masks = []
         labels_for_this_class = [] 
         for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            class_bounding_boxes.append((x, y, x+w, y+h))
-            object_mask = np.zeros_like(mask)
-            cv2.drawContours(object_mask, [contour], -1, 1, -1)
-            class_object_masks.append(object_mask)
-            labels_for_this_class.append(label)
+            contour_area = cv2.contourArea(contour)
+            if contour_area > 0.0:    # sometimes finds 0 dim contours?
+                x, y, w, h = cv2.boundingRect(contour)
+                class_bounding_boxes.append((x, y, x+w, y+h))
+                object_mask = np.zeros_like(mask)
+                cv2.drawContours(object_mask, [contour], -1, 1, -1)
+                class_object_masks.append(object_mask)
+                labels_for_this_class.append(label)
 
         boxes.extend(class_bounding_boxes)
         labels.extend(labels_for_this_class)
         binary_masks.extend(class_object_masks)
-
-    t2 = time.time()
-    print(t2-t1)
 
     boxes = np.array(boxes)
     labels = np.array(labels)
@@ -71,6 +67,3 @@ for i in range(N):
     np.save(os.path.join(example_path, 'boxes.npy'), boxes)
     np.save(os.path.join(example_path, 'masks.npy'), masks)
     np.save(os.path.join(example_path, 'labels.npy'), labels)
-    print(time.time() - t2)
-
-    exit()
